a9684a1ff1aa9141afdaeb04b79637bc703da34f
BelovUser <139882857+BelovUser@users.noreply.github.com>
2023-12-20 14:12:49 +0100

    Created project with packages.

---
+++ AnimeRating/src/test/java/com/example/animerating/AnimeRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animerating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
