70ab45ad8afb876fb4a714793c6b619356f1dfe0
BelovUser <139882857+BelovUser@users.noreply.github.com>
2023-12-21 21:24:02 +0100

    Created simple methods in AnimeService class

--- AnimeRating/src/main/java/com/example/animerating/services/AnimeService.java
+++ AnimeRating/src/main/java/com/example/animerating/services/AnimeService.java
@@ -1,4 +1,32 @@
 package com.example.animerating.services;
 
+import com.example.animerating.models.Anime;
+import com.example.animerating.repositories.AnimeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AnimeService {
+
+    private final AnimeRepository animeRepository;
+
+    @Autowired
+    public AnimeService(AnimeRepository animeRepository) {
+        this.animeRepository = animeRepository;
+    }
+
+    public Optional<Anime> getById(Long id) {
+        return animeRepository.findById(id);
+    }
+
+    public List<Anime> getAll() {
+        return animeRepository.findAll();
+    }
+
+    public void save(Anime anime) {
+        animeRepository.save(anime);
+    }
 }
