af630b1e55347f7a1ca6d84c5c440f1b2924b02d
BelovUser <139882857+BelovUser@users.noreply.github.com>
2023-12-22 16:04:10 +0100

    Created script in main html.

--- AnimeRating/src/main/java/com/example/animerating/services/AnimeService.java
+++ AnimeRating/src/main/java/com/example/animerating/services/AnimeService.java
@@ -1,16 +1,10 @@
 package com.example.animerating.services;
 
-import com.example.animerating.kitsuapicalls.KitsuApiCalls;
 import com.example.animerating.models.Anime;
-import com.example.animerating.models.KitsuAnimeResponse;
 import com.example.animerating.repositories.AnimeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,36 +12,12 @@ import java.util.Optional;
 public class AnimeService {
 
     private final AnimeRepository animeRepository;
-    private KitsuApiCalls kitsuApiCalls;
-    private static final String BASE_URL = "https://kitsu.io/api/edge/";
 
 
     @Autowired
     public AnimeService(AnimeRepository animeRepository) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        kitsuApiCalls = retrofit.create(KitsuApiCalls.class);
-
         this.animeRepository = animeRepository;
     }
-
-
-    public Anime getAnimeById(Long id) {
-        Call<Anime> call = kitsuApiCalls.getAnimeById(id);
-        try {
-            retrofit2.Response<Anime> response = call.execute();
-            if (response.isSuccessful()) {
-                return call.execute().body();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
 
 
     public Optional<Anime> getById(Long id) {
